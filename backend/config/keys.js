b7c61d2b3d0268b1de2fb144ec23e004c785a948
botringo <leohzhou@gmail.com>
2020-07-29 19:31:12 -0700

    Added registration backend

---
+++ backend/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://leo:pass@cluster0.l8hlo.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    secretOrKey: "secret"
+};
---
+++ backend/validation/register.js
@@ -0,0 +1,50 @@
+const Validator = require("validator");
+const isEmpty = require("is-empty");
+
+module.exports = function validateRegisterInput(data) {
+    let errors = {};
+
+    // Empty field -> ""
+    data.email = !isEmpty(data.email) ? data.email : "";
+    data.password = !isEmpty(data.password) ? data.password : "";
+    data.firstName = !isEmpty(data.firstName) ? data.firstName : "";
+    data.lastName = !isEmpty(data.lastName) ? data.lastName : "";
+    data.phoneNumber = !isEmpty(data.phoneNumber) ? data.phoneNumber : "";
+
+    // Email checks
+    if (Validator.isEmpty(data.email)) {
+        errors.email = "Email field is required";
+    } else if (!Validator.isEmail(data.email)) {
+        errors.email = "Email is invalid";
+    }
+
+    // Password checks
+    if (Validator.isEmpty(data.password)) {
+        errors.password = "Password field is required";
+    } else if (!Validator.isLength(data.password, {
+            min: 6,
+            max: 30
+        })) {
+        errors.password = "Password must be at least 6 characters";
+    }
+
+    // Name checks
+    if (Validator.isEmpty(data.firstName)) {
+        errors.name = "First Name field is required";
+    }
+    if (Validator.isEmpty(data.lastName)) {
+        errors.name = "Last Name field is required";
+    }
+
+    // Phone number checks
+    if (Validator.isEmpty(data.phoneNumber)) {
+        errors.name = "Phone Number field is required";
+    } else if (!Validator.isMobilePhone(data.phoneNumber)) {
+        errors.name = "Phone Number is invalid"
+    }
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    };
+};
